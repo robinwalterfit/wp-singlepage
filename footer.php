4975095d91f9b8c6764aa3e627686fb0b73c1060
Robin Walter <r.walter12@outlook.com>
2017-04-30 20:12:04 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php
+    
+    /**
+     * The footer for the theme
+     *
+     * @package SinglePage
+     * @since 1.0.0
+     * @version 1.0.0
+     */
+
+?>
+
+
+                <footer class="container-fluid" id="site-footer" role="contentinfo">
+                
+                
+                
+                </footer><!-- #site-footer.container-fluid -->
+                
+            </div><!-- #site-wrapper -->
+            
+            <?php wp_footer(); ?>
+            
+        </body>
+        
+    </html><!-- .no-js -->
